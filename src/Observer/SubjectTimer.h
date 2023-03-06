59c44f5cf7ebfffe907becd84021e5b99b11af09
Elton Silva <eltoncarsilva73@gmail.com>
2023-03-06 20:52:02 -0300

    subject timer fix

--- src/Observer/SubjectTimer.h
+++ src/Observer/SubjectTimer.h
@@ -5,32 +5,35 @@
 #include "../Entities/GameObject.h"
 #include "../Scene/Scene.h"
 
-class SubjectTimer : public GameObject
+class SubjectTimer : public GameObject, public IObserver
 {
 private:
     float _timer;
     bool _isDone = false;
+    bool _isStarted = false;
 
 public:
     ISubject *OnRiseEvent = new ISubject();
 
-    SubjectTimer(float timer, Scene *scene)
-    {
-        _timer = timer;
-        scene->AddUI(this);
-    }
+    SubjectTimer(float timer) { _timer = timer; }
 
     void virtual Update(float dt) override
     {
         if (_isDone)
             return;
 
+        if (!_isStarted)
+            return;
+
         _timer -= dt;
         if (_timer <= 0)
+        {
             _isDone = true;
-
-        OnRiseEvent.Notify();
+            OnRiseEvent->Notify();
+        }
     }
+
+    void virtual Notify() override { _isStarted = true; }
 };
 
 #endif
