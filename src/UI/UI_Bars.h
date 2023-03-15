9cebdc65402802919a64b9dfc5decd71a0638c97
Elton Silva <eltoncarsilva73@gmail.com>
2023-03-15 19:59:02 -0300

    life bar hit effect

--- src/UI/UI_Bars.h
+++ src/UI/UI_Bars.h
@@ -8,16 +8,31 @@
 class UI_Bars : public GameObject, public IObserver
 {
 private:
-    Texture2D _sprite{};
     const float MAX_TIME_SHAKE = 1.5f;
+    float SPEED_BAR_EFFECT = 30.0f;
+
+    Texture2D _sprite{};
     float _shakeTime = 0;
+
+    Vector2 _barSizeHP = {88, 7};
+    Vector2 _barSizeHPWhite = {88, 7};
+    Vector2 _barHPPosition = {34, 9};
+
+    Vector2 _barSizeLight = {69, 8};
+    Vector2 _barLightPosition = {34, 18};
+
+    float _lastHPValue = 0;
 
 public:
     virtual void Start() override;
     virtual void Update(float dt) override;
     virtual void Draw() override;
 
-    virtual void Notify() override { _shakeTime = MAX_TIME_SHAKE; }
+    virtual void Notify() override
+    {
+        _shakeTime = MAX_TIME_SHAKE;
+        _barSizeHPWhite = {_barSizeHP.x * _lastHPValue, _barSizeHPWhite.y};
+    }
 };
 
 #endif
